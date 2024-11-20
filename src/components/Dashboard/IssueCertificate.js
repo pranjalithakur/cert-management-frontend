e8dee0f14dcf883c8251e6416943ad572b33944c
pranjalithakur <thakur.pranjali19@gmail.com>
2024-11-19 23:30:15 -0500

    integrate ipfs storage

--- src/components/Dashboard/IssueCertificate.js
+++ src/components/Dashboard/IssueCertificate.js
@@ -7,6 +7,8 @@ import { getAssociatedTokenAddress } from "@solana/spl-token";
 import idl from '../../solana_nft_anchor_hackernoon.json';
 import { SolanaNftAnchorHackernoon } from '../../solana_nft_anchor_hackernoon'
 import '../../styles/IssuePage.css';
+import { PinataSDK } from "pinata-web3";
+
 
 import { walletAdapterIdentity } from "@metaplex-foundation/umi-signer-wallet-adapters";
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
@@ -27,10 +29,15 @@ window.Buffer = Buffer;
 
 
 const network = process.env.REACT_APP_SOLANA_NETWORK || clusterApiUrl("devnet");
-// const programID = new PublicKey(process.env.REACT_APP_PROGRAM_ID);
 const { SystemProgram, Keypair } = web3;
 const programID = new PublicKey(idl.metadata.address);
-console.log(programID);
+
+const pinata = new PinataSDK({
+  pinataJwt: process.env.REACT_APP_PINATA_JWT,
+  pinataGateway: process.env.REACT_APP_PINATA_GATEWAY,
+});
+
+const GATEWAY_URL = "https://gateway.pinata.cloud/ipfs/";
 
 const IssuePage = () => {
   const wallet = useWallet();
@@ -39,11 +46,53 @@ const IssuePage = () => {
     description: "",
     issueDate: "",
     recipientAddress: "",
+    image: null,
   });
+
+  const [loading, setLoading] = useState(false);
+  const [imageFile, setImageFile] = useState(null);
+  const [ipfsImageLink, setIpfsImageLink] = useState("");
+  const [metadataLink, setMetadataLink] = useState("");
+  
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleImageUpload = (e) => {
+    const file = e.target.files[0];
+    setImageFile(file);
+  };
+
+  const uploadImageToIPFS = async (image) => {
+    try { 
+      // Upload the image to Pinata
+      const upload = await pinata.upload.file(image)
+      console.log("Uploaded:", upload);
+
+      const ipfsUrl = `https://teal-dry-albatross-975.mypinata.cloud/ipfs/${upload.IpfsHash}`;   // hardcoded gateway
+      console.log(ipfsUrl)
+      return ipfsUrl
+    } catch (error) {
+      console.error("Error uploading to IPFS:", error);
+      alert("Failed to upload image to IPFS.");
+    }
+  };
+
+  const uploadMetadataToIPFS = async (metadata) => {
+    try {
+      const metadataBlob = new Blob([JSON.stringify(metadata)], {type: 'application/json'});
+      const file = new File([metadataBlob], "metadata.json");
+      const upload = await pinata.upload.file(file);
+      const ipfsMetadataUrl = `https://teal-dry-albatross-975.mypinata.cloud/ipfs/${upload.IpfsHash}`;
+      console.log("Uploaded metadata to IPFS:", ipfsMetadataUrl);
+      return ipfsMetadataUrl
+      
+    } catch (error) {
+      console.error("Error uploading metadata to IPFS:", error);
+      alert("Failed to upload metadata to IPFS.");
+    }
   };
 
   const handleSubmit = async(e) => {
@@ -54,7 +103,13 @@ const IssuePage = () => {
       return;
     }
 
+    if (!imageFile) {
+      alert("Please upload an image.");
+      return;
+    }
+
     try {
+// -------------------------------------------------------------------------------------------------------------
       console.log("Establishing connection...");
       const connection = new Connection(network, "processed");
       const provider = new AnchorProvider(connection, wallet, {
@@ -80,12 +135,40 @@ const IssuePage = () => {
       } catch (error) {
         console.error("Error initializing program:", error);
       }
+// ------------------------------------------------------------------------------------------------------------- 
+      // Upload the image to IPFS
+      console.log("Uploading image to Pinata...");
+      const imageUri = await uploadImageToIPFS(imageFile);
+      // console.log("Image uploaded to IPFS at:", imageUri);
+      setIpfsImageLink(imageUri)
+      // Create metadata for upload
 
-      const signer = provider.wallet;
-      
-      const umi = createUmi("https://api.devnet.solana.com")
-        .use(walletAdapterIdentity(signer))
-        .use(mplTokenMetadata());
+      // Create metadata object
+      const metadata = {
+        name: formData.certName,
+        symbol: formData.certSymbol,   // formData.symbol
+        description: formData.description,
+        image: imageUri,
+        attributes: [
+          { trait_type: "Issuer", value: wallet.publicKey.toString() },  // value: formData.issuer
+          { trait_type: "Recipient", value: formData.recipientAddress },   // value: formData.recipient
+          { trait_type: "Date of Issue", value: formData.issueDate },   // value: formData.dateOfIssue
+          { trait_type: "Program", value: "cybersec" },   // value: formData.program
+        ],
+        properties: {
+          category: "image",
+          files: [{ uri: imageUri, type: imageFile.type }],
+        },
+      };
+
+      // Upload the metadata to IPFS
+      console.log("Uploading metadata to IPFS...");
+      const metadataUri = await uploadMetadataToIPFS(metadata);
+      setMetadataLink(metadataUri);
+      console.log("Metadata CID:", metadataUri);
+      alert("Image and metadata uploaded successfully!");
+
+// -------------------------------------------------------------------------------------------------------------
      
       // Generate new keypair for mint account
       const mintKeypair = Keypair.generate();
@@ -99,36 +182,11 @@ const IssuePage = () => {
         ASSOCIATED_TOKEN_PROGRAM_ID
       );
       console.log("Associated Token Account:", associatedTokenAccount.toString());
-      // const associatedTokenAddress = await web3.PublicKey.findProgramAddress(
-      //   [
-      //     wallet.publicKey.toBuffer(),
-      //     TOKEN_PROGRAM_ID.toBuffer(),
-      //     mintKeypair.publicKey.toBuffer(),
-      //   ],
-      //   ASSOCIATED_TOKEN_PROGRAM_ID
-      // );
 
-      // derive the metadata PDA
-      // const [metadataAccount] = PublicKey.findProgramAddressSync(
-      //   [
-      //     Buffer.from("metadata"),
-      //     MPL_TOKEN_METADATA_PROGRAM_ID.toBuffer(),
-      //     mintKeypair.publicKey.toBuffer(),
-      //   ],
-      //   MPL_TOKEN_METADATA_PROGRAM_ID
-      // );
-      // console.log("Metadata Account:", metadataAccount.toString());
-
-      // //derive the master edition pda
-      // const [masterEditionAccount] = PublicKey.findProgramAddressSync(
-      //   [
-      //     Buffer.from("metadata"),
-      //     MPL_TOKEN_METADATA_PROGRAM_ID.toBuffer(),
-      //     mintKeypair.publicKey.toBuffer(),
-      //     Buffer.from("edition"),
-      //   ],
-      //   MPL_TOKEN_METADATA_PROGRAM_ID
-      // );
+      const signer = provider.wallet;
+      const umi = createUmi("https://api.devnet.solana.com")
+        .use(walletAdapterIdentity(signer))
+        .use(mplTokenMetadata());
 
       // derive the metadata account
       let metadataAccount = findMetadataPda(umi, {
@@ -141,16 +199,17 @@ const IssuePage = () => {
       })[0];
 
       // Sample metadata
-      const metadata = {
-        name: "Kobeni",
-        symbol: "kBN",
-        uri: "https://raw.githubusercontent.com/687c/solana-nft-native-client/main/metadata.json",
+      const metadata_main = {
+        name: formData.certName,
+        symbol: formData.certSymbol,
+        uri: metadataUri,
       };
       
       
       // Call smart contract to mint certificate NFT
+      console.log("Minting NFT...");
       const tx = await program.methods
-        .initNft(metadata.name, metadata.symbol, metadata.uri)
+        .initNft(metadata_main.name, metadata_main.symbol, metadata_main.uri)
         .accounts({
           signer: wallet.publicKey,
           mint: mintKeypair.publicKey,
@@ -182,7 +241,7 @@ const IssuePage = () => {
     } catch (error) {
       console.error("Error issuing certificate:", error);
       alert("Failed to issue certificate.");
-    }
+    } 
   };
   
 
@@ -196,6 +255,15 @@ const IssuePage = () => {
           name="certName"
           placeholder="Certificate Name"
           value={formData.certName}
+          onChange={handleChange}
+          required
+        />
+        <input
+          className="form-input"
+          type="text"
+          name="certSymbol"
+          placeholder="Certificate Symbol"
+          value={formData.certSymbol}
           onChange={handleChange}
           required
         />
@@ -224,6 +292,14 @@ const IssuePage = () => {
           placeholder="Recipient Wallet Address"
           value={formData.recipientAddress}
           onChange={handleChange}
+          required
+        />
+        <input
+          className="form-input"
+          type="file"
+          name="image"
+          accept="image/*"
+          onChange={handleImageUpload}
           required
         />
         <button className="submit-btn" type="submit">Issue Certificate</button>
